98330be0017b6d7d46f13f6137b513c0ea8c6a08
Lightning-blade <allfsocial@protonmail.ch>
2022-05-23 16:02:14 +0200

    Changes to api.py and model.py

--- model.py
+++ model.py
@@ -58,7 +58,16 @@ def _preprocess_data(data):
     # ---------------------------------------------------------------
 
     # ----------- Replace this code with your own preprocessing steps --------
-    predict_vector = feature_vector_df[['Madrid_wind_speed','Bilbao_rain_1h','Valencia_wind_speed']]
+    feature_vector_df['Valencia_pressure']=feature_vector_df['Valencia_pressure'].fillna(feature_vector_df['Valencia_pressure'].mode())
+    feature_vector_df['time'] = pd.to_datetime(feature_vector_df['time'], format='%Y-%m-%d %H:%M:%S')
+    feature_vector_df['year'] = feature_vector_df['time'].dt.year
+    feature_vector_df['month'] = feature_vector_df['time'].dt.month
+    feature_vector_df['day'] = feature_vector_df['time'].dt.day
+    feature_vector_df['hour'] = feature_vector_df['time'].dt.hour
+    feature_vector_df['minute'] = feature_vector_df['time'].dt.minute
+    removable = ['time', 'Unnamed: 0', 'Seville_pressure', 'Valencia_wind_deg', 'Valencia_pressure']
+    feature_vector_df = feature_vector_df.drop(removable, axis=1)
+    predict_vector = feature_vector_df
     # ------------------------------------------------------------------------
 
     return predict_vector
